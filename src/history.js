32aa78361f268e93866650f3c082f715f012b707
Siegfred Balona <siegblink@gmail.com>
2019-03-03 11:17:57 +0800

    Load complete CRUD functionalities

---
+++ src/history.js
@@ -0,0 +1,7 @@
+import createHistory from 'history/createBrowserHistory';
+
+// Create our own history object from createHistory function
+// So we can implement a programmatic navigation
+const history = createHistory();
+
+export default history;
